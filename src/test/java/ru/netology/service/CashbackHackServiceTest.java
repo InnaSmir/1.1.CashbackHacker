d2bc7a16c2e5a59084289b6f10c0db857ddb74b1
Inna Smirnova <moipr@yandex.ru>
2020-08-03 02:36:25 +0300

    Renamed tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,27 +1,34 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldRemainIfAmount0() {
+
         assertEquals(service.remain(0), 1000);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldRemainIfAmount1000() {
+
         assertEquals(service.remain(1000), 0);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldRemainIfAmount500() {
+
         assertEquals(service.remain(500), 500);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldRemainIfAmount999() {
+
         assertEquals(service.remain(999), 1);
     }
 
