eff2e6aee676e8b55bcdae1aab6227a485ffcdf2
Inna Smirnova <moipr@yandex.ru>
2020-08-03 02:19:34 +0300

    Added service tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,30 @@
 package ru.netology.service;
 
+import ru.netology.service.service.CashbackHackService;
+
+import static org.testng.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmount0() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmount1000() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmount500() {
+        assertEquals(service.remain(500), 500);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldRemainIfAmount999() {
+        assertEquals(service.remain(999), 1);
+    }
+
 }
